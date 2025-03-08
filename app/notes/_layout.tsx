5fb6a87b88659c3a66656e07657b4bc2e940240b
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-03-08 16:33:43 +0300

    Created components to make the Notes page to be neater

---
+++ app/notes/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+const NotesLayout = () => {
+  return <Stack
+
+      screenOptions={{
+        headerShown: false,
+      }}
+  />
+}
+ export default NotesLayout;
